420152bc13d6f3875f2de98fd4c908345e06085b
PisecesPeng <Piseces.Peng@gmail.com>
2018-01-23 22:11:24 +0800

    add .sql

---
+++ src/main/resources/config/db/Quarusis所有表.sql
@@ -0,0 +1,42 @@
+-- 增加用户信息表
+CREATE TABLE `user_info` (
+  `uin` VARCHAR(10) NOT NULL COMMENT '微信UIN',
+  `name` VARCHAR(10) NOT NULL COMMENT '用户名称',
+  PRIMARY KEY (uin)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '用户信息表';
+
+-- 增加用户评论历史表
+CREATE TABLE `#{uin}_history` (
+  `page_id` VARCHAR(10) NOT NULL COMMENT 'pageID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '用户评论历史表';
+
+-- 增加用户page表
+CREATE TABLE `user_page` (
+  `page_id` INT(4) NOT NULL AUTO_INCREMENT COMMENT 'pageID - 自增',
+  `uin` VARCHAR(10) NOT NULL COMMENT '微信UIN',
+  `page_topic` VARCHAR(3) NOT NULL COMMENT 'page话题',
+  `page_title` VARCHAR(20) NOT NULL COMMENT 'page标题',
+  `page_text` VARCHAR(70) NOT NULL COMMENT 'page内容',
+  `resource_url` VARCHAR(50) DEFAULT NULL COMMENT 'page资源url',
+  `whether_comment` TINYINT DEFAULT 1 COMMENT '是否允许评论，默认允许',
+  `comment_sum` INT(4) DEFAULT 0 COMMENT '统计评论数量',
+  `whether_read` TINYINT DEFAULT 0 COMMENT '是否已查看最新评论，默认已查看',
+  PRIMARY KEY (`page_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '用户page表';
+
+-- 增加指定page评论表
+CREATE TABLE `#{page_id}_comment` (
+  `comment_id` INT(4) NOT NULL AUTO_INCREMENT COMMENT 'page的commentID - 自增',
+  `uin` VARCHAR(10) NOT NULL COMMENT '微信UIN',
+  `comment_text` VARCHAR(20) NOT NULL COMMENT 'comment内容',
+  `comment_heat` INT(4) DEFAULT 0 COMMENT 'comment热度',
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '指定page评论表';
+
+-- 增加评论热度表，判断用户是否已经heat
+CREATE TABLE `heat` (
+  `uin` VARCHAR(10) NOT NULL COMMENT '微信UIN',
+  `page_id` INT(4) NOT NULL  COMMENT 'page的ID',
+  `comment_id` INT(4) NOT NULL COMMENT 'page的commentID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '评论热度表';
+
