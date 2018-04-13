d015f7ca40d827c37e9cec5a5781a69d7468501d
PisecesPeng <Piseces.Peng@gmail.com>
2018-04-13 19:56:41 +0800

    add some ideas

--- src/main/java/com/quarusis/service/impl/PageServiceImpl.java
+++ src/main/java/com/quarusis/service/impl/PageServiceImpl.java
@@ -101,4 +101,63 @@ public class PageServiceImpl implements PageService{
         logger.info("removePage(Integer) - end");
     }
 
+    /**
+     * 评论其他人评论的功能(可以替代原评论功能commentUpload方法)
+     * 只是一个新想法，暂时只写service层
+     */
+//    @Transactional
+//    public void commentaryReview(Comment comment) throws Exception {
+
+        // 略去增加评论总数(plusPageCommentSum())、刷新评论历史(deleteCommentHistory()、insertCommentHistory())等步骤
+
+//        uin 评论人自己的uin
+//        toUin 被评论的人的uin
+//        id 子评论id(递增)
+//        toId 母评论(若为0 则为该一系列评论对话的第一条评论，剩余评论若要加入评论对话，必须将toId设置为母评论的id)
+//        以下的数据都可get
+        /*
+         先判断 toId 是否为 0
+         若 为0
+            则使用 id 作为 新行的toId
+            插入新行
+                toComment_id = this.id
+                uin = this.uin
+                toUin = this.toUin
+                comment_text = this.comment_text
+         若 非0
+            则使用 toId
+            插入新行
+                toComment_id = this.toId
+                uin = this.uin
+                toUin = this.toUin
+                comment_text = this.comment_text
+          */
+
+//    }
+
+    /**
+     * 评论其他人评论的功能(可以替代原评论功能commentUpload方法)
+     * 只是一个新想法，暂时只写service层
+     */
+//    public List<Comment> commentaryDialogue(Comment comment) throws Exception {
+
+//        uin 评论人自己的uin
+//        toUin 被评论的人的uin
+//        id 子评论id(递增)
+//        toId 母评论(若为0 则为该一系列评论对话的第一条评论，剩余评论若要加入评论对话，必须将toId设置为母评论的id)
+//        以下的数据都可get
+//
+        // 返回的List 为母评论与子评论拼装得来
+        /*
+         先判断 toId 是否为 0
+         若 为0
+            则此评论为顶级母评论，将其添加入List
+            将 id 作为 toId 查询剩余 子评论(子评论的 toId == 母评论的 id)，添加入List
+         若 非0
+            则将 toId 作为 id 查询母评论，将其添加入List
+            将 toId 作为 toId 查询所有子评论，将其添加入List
+          */
+
+//    }
+
 }
